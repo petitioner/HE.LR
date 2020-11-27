0782f2317c0ab35934d431c6160ecb6adc53d380
petitioner <51513317+petitioner@users.noreply.github.com>
2020-11-27 19:55:11 +0800

    Add files via upload

---
+++ IDASH2017/Default/src/MyMethods.d
@@ -0,0 +1,133 @@
+src/MyMethods.o: ../src/MyMethods.cpp ../src/MyMethods.h ../src/MyTools.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Scheme.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/RR.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ZZ.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/lip.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ctools.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/config.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/mach_desc.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ALL_FEATURES.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_ALIGNED_ARRAY.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_BUILTIN_CLZL.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_LL_TYPE.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_SSSE3.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_AVX.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_PCLMUL.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_AVX2.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_FMA.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_AVX512F.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_COPY_TRAITS1.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_COPY_TRAITS2.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_CHRONO_TIME.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_MACOS_TIME.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_POSIX_TIME.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/PackageInfo.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/gmp_aux.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/tools.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/new.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vector.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/SmartPtr.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/thread.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/sp_arith.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/xdouble.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/quad_float.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/BootContext.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/SecretKey.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Ring.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/RingMultiplier.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Params.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Ciphertext.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Plaintext.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Key.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/EvaluatorUtils.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Ciphertext.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ZZX.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vec_ZZ.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/lzz_pX.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/lzz_p.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/FFT.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vec_long.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/LazyTable.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vec_lzz_p.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/Lazy.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/mat_lzz_p.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/matrix.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vec_vec_lzz_p.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ZZ_pX.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ZZ_p.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ZZVec.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vec_ZZ_p.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/mat_ZZ_p.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vec_vec_ZZ_p.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/TestScheme.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/SecretKey.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/TimeUtils.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/EvaluatorUtils.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/BasicThreadPool.h
+../src/MyMethods.h:
+../src/MyTools.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Scheme.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/RR.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ZZ.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/lip.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ctools.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/config.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/mach_desc.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ALL_FEATURES.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_ALIGNED_ARRAY.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_BUILTIN_CLZL.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_LL_TYPE.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_SSSE3.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_AVX.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_PCLMUL.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_AVX2.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_FMA.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_AVX512F.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_COPY_TRAITS1.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_COPY_TRAITS2.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_CHRONO_TIME.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_MACOS_TIME.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_POSIX_TIME.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/PackageInfo.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/gmp_aux.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/tools.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/new.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vector.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/SmartPtr.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/thread.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/sp_arith.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/xdouble.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/quad_float.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/BootContext.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/SecretKey.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Ring.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/RingMultiplier.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Params.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Ciphertext.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Plaintext.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Key.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/EvaluatorUtils.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/Ciphertext.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ZZX.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vec_ZZ.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/lzz_pX.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/lzz_p.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/FFT.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vec_long.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/LazyTable.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vec_lzz_p.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/Lazy.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/mat_lzz_p.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/matrix.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vec_vec_lzz_p.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ZZ_pX.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ZZ_p.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ZZVec.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vec_ZZ_p.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/mat_ZZ_p.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vec_vec_ZZ_p.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/TestScheme.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/SecretKey.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/TimeUtils.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src/EvaluatorUtils.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/BasicThreadPool.h:
---
+++ IDASH2017/Default/src/MyTools.d
@@ -0,0 +1,58 @@
+src/MyTools.o: ../src/MyTools.cpp ../src/MyTools.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ZZ.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/lip.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ctools.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/config.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/mach_desc.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ALL_FEATURES.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_ALIGNED_ARRAY.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_BUILTIN_CLZL.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_LL_TYPE.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_SSSE3.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_AVX.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_PCLMUL.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_AVX2.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_FMA.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_AVX512F.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_COPY_TRAITS1.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_COPY_TRAITS2.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_CHRONO_TIME.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_MACOS_TIME.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_POSIX_TIME.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/PackageInfo.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/gmp_aux.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/tools.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/new.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vector.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/SmartPtr.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/thread.h \
+ /home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/sp_arith.h
+../src/MyTools.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ZZ.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/lip.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ctools.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/config.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/mach_desc.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/ALL_FEATURES.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_ALIGNED_ARRAY.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_BUILTIN_CLZL.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_LL_TYPE.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_SSSE3.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_AVX.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_PCLMUL.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_AVX2.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_FMA.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_AVX512F.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_COPY_TRAITS1.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_COPY_TRAITS2.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_CHRONO_TIME.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_MACOS_TIME.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/HAVE_POSIX_TIME.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/PackageInfo.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/gmp_aux.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/tools.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/new.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/vector.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/SmartPtr.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/thread.h:
+/home/sunly/Downloads/IDASH2017-master/IDASH2017/lib/include/NTL/sp_arith.h:
